c2f8c12d1fc82f0deb6a5057b42e9d714944eac6
Jagritidas123 <jagritid764@gmail.com>
2025-07-26 23:36:20 +0530

    Add files via upload

---
+++ Code.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Jagriti\n" << "Das\n";
+    return 0;
+    // Comments- the non executable part of the code
+}
---
+++ Variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age = 23;
+    float PI = 3.14f;
+    char grade = 'A';
+    bool issafe = false;
+    double price = 100.99;
+    cout <<  issafe<< endl;
+    return 0;
+    
+}
