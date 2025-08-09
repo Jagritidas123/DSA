7d99ecabfc9b007a5c23d71e2056ddfc532de979
Jagritidas123 <jagritid764@gmail.com>
2025-08-09 11:17:18 +0530

    Add files via upload

---
+++ Datatypevariable.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    cout << a << endl;
+
+    char c = 'C';
+    cout << c << endl;
+
+    bool bl = false;
+    cout << bl << endl;
+
+    float f = 12.3;
+    cout << f<< endl;
+
+    double d = 2.22;
+    cout << d << endl;
+    // SEEING HOW MUCH MEMORY SPACE CAN BE TAKEN BY A DATATYPE
+int size = sizeof(d);
+cout << size << endl;
+
+}
