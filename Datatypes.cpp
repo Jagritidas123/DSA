ce60c9620ee4885dd27040693167c47a67350480
Jagritidas123 <jagritid764@gmail.com>
2025-09-05 20:58:34 +0530

    Add files via upload

---
+++ Datatypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 12;
+    cout << a << endl;
+
+    char b = 'V';
+    cout << b << endl;
+
+    bool bl = true;
+    cout << bl <<endl;
+
+    double D = 1.22;
+    cout << D << endl;
+    // To check the size of anything
+    int size = sizeof(a);
+     cout << "The size of a is : " << size << endl;
+
+}
