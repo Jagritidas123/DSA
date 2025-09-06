b069db3f8710ec51af2dc853f0d78cb33abc6ce5
Jagritidas123 <jagritid764@gmail.com>
2025-09-06 11:44:11 +0530

    Add files via upload

---
+++ Conditionals.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a ;
+    cin >> a;
+    if (a > 0){
+        cout << "It is positive"<< endl;
+
+    }
+    else{
+        cout << "It is negative" << endl;
+    }
+}
