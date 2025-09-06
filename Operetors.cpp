79716466bf68fabb99d746125465551e614d2e14
Jagritidas123 <jagritid764@gmail.com>
2025-09-06 11:18:28 +0530

    Add files via upload

---
+++ Operetors.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // RELETIONAL OPERETORS
+    int a= 2;
+    int b = 3;
+    bool one = (a==b);
+    cout << one << endl;
+    bool two = (a>=b);
+    cout << two << endl;
+    bool three = (a<=b);
+    cout << three << endl;
+    bool four = (a>b);
+    cout << four << endl;
+    bool five = (a<b);
+    cout << five << endl;
+    bool six =(a!=b);
+    cout << six << endl;
+
+    
+
+
+}
